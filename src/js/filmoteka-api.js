60dedb3bad3eeada681aaf1e689c3543db90d4da
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-01-28 22:59:37 +0200

    added film-prewiev template

---
+++ src/js/filmoteka-api.js
@@ -0,0 +1,11 @@
+// const API_KEY = '219747bddc830c6768a55001e81d80ed';
+// function name() {
+//   fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
+//     .then(res => {
+//       res.json();
+//     })
+//     .then(res => {
+//       console.log(res);
+//     });
+// }
+// name();
