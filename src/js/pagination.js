0f0f89c54a0faa46c7291a88f19f7dadcf4341bc
Lidija Levchenko <levchenkola@ukr.net>
2022-01-29 00:11:14 +0200

    =

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,57 +1,45 @@
-// import Pagination from 'tui-pagination';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
 
-// // import { trendingMovies, getGenres } from './js/apiService';
-
-// // paginationRef: document.getElementById('tui-pagination'),
-// // export function  renderPaginationTrendigMovie(totalItems) {
-
-//  const container = document.getElementById('.tui-pagination');
-// const options = { // below default value of options
-//      totalItems: 10,
-//      itemsPerPage: 10,
-//      visiblePages: 10,
-//      page: 1,
-//      centerAlign: false,
-//      firstItemClassName: 'tui-first-child',
-//      lastItemClassName: 'tui-last-child',
-//      template: {
-//          page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-//          currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-//          moveButton:
-//              '<a href="#" class="tui-page-btn tui-{{type}}">' +
-//                  '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//              '</a>',
-//          disabledMoveButton:
-//              '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-//                  '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//              '</span>',
-//          moreButton:
-//              '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-//                  '<span class="tui-ico-ellip">...</span>' +
-//              '</a>'
-//      }
-// };
-
-// //   const pagination = new tui.Pagination('pagination', {
-// //     totalItems: 500
-// // });
-
-// // pagination.on('beforeMove', function(eventData) {
-// //     return confirm('Go to page ' + eventData.page + '?');
-// // });
-
-// // pagination.on('afterMove', function(eventData) {
-// //     alert('The current page is ' + eventData.page);
-// // });
-
-// const pagination = new Pagination(container, options);
-//   pagination.reset();
-//   pagination.on('beforeMove', function (eventData) {
-//     dataFetch.page = eventData.page;
-//     dataFetch.fetchTopFilms().then(films => {
-//       refs.galleryRef.innerHTML = '';
-//       renderMovieCardFilms(films);
-//     });
-//   });
+// 
 
 
+export function  renderPaginationTrendigMovie(totalItems) {
+
+ const container = document.getElementById('tui-pagination-container');
+//  const instance = new Pagination(container, options);
+const options = { // below default value of options
+     totalItems: 10,
+     itemsPerPage: 10,
+     visiblePages: 10,
+     page: 1,
+     centerAlign: false,
+     firstItemClassName: 'tui-first-child',
+     lastItemClassName: 'tui-last-child',
+     template: {
+         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+         moveButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</a>',
+         disabledMoveButton:
+             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</span>',
+         moreButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+                 '<span class="tui-ico-ellip">...</span>' +
+             '</a>'
+     }
+};
+const instance = new Pagination(container, options);
+// instance.getCurrentPage();
+instance.on('beforeMove', (event) => {
+    const currentPage = event.page;
+
+    if (currentPage === 10) {
+        return false;
+        // return true;
+    }
+});}
