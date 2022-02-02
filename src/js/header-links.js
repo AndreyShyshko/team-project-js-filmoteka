ba7c9289b344d5408282e19de7bf4acdd6d8fdf1
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-02-02 04:02:25 +0200

    feature/added class isActive and eventListeners for QueueBtn and WatchedBtn

--- src/js/header-links.js
+++ src/js/header-links.js
@@ -1,13 +1,16 @@
 const homeBtn = document.querySelector('#headerHomeBtn');
 const libraryBtn = document.querySelector('#headerLibraryBtn');
-
 const library = document.querySelector('.library-container');
 const queue = document.querySelector('.queue');
+const queueBtn = document.querySelector('#btn__header-queue');
 const watched = document.querySelector('.watched');
+const watchedBtn = document.querySelector('#btn__header-watched');
 import { moviesGallery } from './main-trending-markup.js';
 
 homeBtn.addEventListener('click', goHome);
 libraryBtn.addEventListener('click', goLibrary);
+queueBtn.addEventListener('click', goQueue);
+watchedBtn.addEventListener('click', goWatched);
 
 function goHome() {
   library.classList.add('isHide');
@@ -17,6 +20,21 @@ function goHome() {
 
 function goLibrary() {
   library.classList.remove('isHide');
-  queue.classList.remove('isHide');
   moviesGallery.classList.add('isHide');
+  queue.classList.remove('isHide');
+  queueBtn.classList.add('isActive');
+}
+
+function goQueue() {
+  queueBtn.classList.add('isActive');
+  watchedBtn.classList.remove('isActive');
+  queue.classList.remove('isHide');
+  watched.classList.add('isHide');
+}
+
+function goWatched() {
+  watchedBtn.classList.add('isActive');
+  queueBtn.classList.remove('isActive');
+  queue.classList.add('isHide');
+  watched.classList.remove('isHide');
 }
