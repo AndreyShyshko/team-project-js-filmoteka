07cb80ac2af198b4eda6f25fef165e3197e46f83
igormos1 <gior_gt@ukr.net>
2022-02-02 22:18:12 +0200

    foot

--- src/index.js
+++ src/index.js
@@ -7,5 +7,7 @@ import './js/filmoteka-api';
 import './js/change-genres-id';
 import './js/main-trending-markup';
 import './js/header-links';
+import './js/teamLightbox';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 
 renderPaginationTrendigMovie(1000);
