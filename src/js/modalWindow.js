7c9e30fd3630d84bf47d0a425ade74ffeeb62a3d
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-02-09 03:42:55 +0200

    added check for exist movies in library

--- src/js/modalWindow.js
+++ src/js/modalWindow.js
@@ -1,5 +1,4 @@
 import markupContentModal from '../templates/modalWindow.hbs';
-
 const gallery = document.querySelector('#movies-gallery-container');
 const body = document.querySelector('body');
 const contentModal = document.querySelector('.content-modal');
@@ -11,17 +10,32 @@ oneFilmOwerlay.addEventListener('click', closeFilmModal);
 
 function openModalWindow(evt) {
   const moviesArr = JSON.parse(localStorage.getItem('fetched-movies-array'));
+  const watchedMoviesList = JSON.parse(localStorage.getItem('watched'));
+  const queueMoviesList = JSON.parse(localStorage.getItem('queue'));
+  const movie_id = evt.target.attributes['data-action'].nodeValue;
+  const movieInfo = moviesArr.find(movie => movie.id === +movie_id);
+  const movieExistsInWatched = watchedMoviesList.find(movie => movie.id === +movie_id);
+  const movieExistsInQueue = queueMoviesList.find(movie => movie.id === +movie_id);
+
   if (evt.target.nodeName !== 'IMG') {
     return;
   }
 
   contentHidden();
 
-  const movie_id = evt.target.attributes['data-action'].nodeValue;
-  const movieInfo = moviesArr.find(movie => movie.id === +movie_id);
-
   const markup = markupContentModal(movieInfo);
   contentModal.insertAdjacentHTML('afterbegin', markup);
+
+  const addToWatchedBtn = document.querySelector('.add-to-watched');
+  const addToQueueBtn = document.querySelector('.add-to-queue');
+
+  if (movieExistsInWatched) {
+    addToWatchedBtn.innerHTML = 'Remove from watched';
+  }
+
+  if (movieExistsInQueue) {
+    addToQueueBtn.innerHTML = 'Remove from queue';
+  }
 
   modalWindow.classList.add('open');
 }
